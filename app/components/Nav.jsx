9ba52a5b88ec3a2806887bfe4a95cbe9a33eb6fb
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-03-31 22:44:58 +0200

    Added right part of Navbar with Search functionality

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,8 +1,12 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return (
+var Nav = React.createClass({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert("Not yet wired up")
+  },
+  render: function () {
     <div className="top-bar">
       <div className="top-bar-left">
         <ul className="menu">
@@ -13,10 +17,20 @@ var Nav = () => {
         </ul>
       </div>
       <div className="top-bar-right">
+        <form onSubmit={this.onSearch}>
+          <ul className="menu">
+            <li>
+              <input type="search" placeholder="Search Weather"/>
+            </li>
+            <li>
+              <input type="submit" class="button" value="Get Weather"/>
+            </li>
+          </ul>
+        </form>
       </div>
     </div>
-  );
-};
+  }
+});
 
 module.exports = Nav;
 
