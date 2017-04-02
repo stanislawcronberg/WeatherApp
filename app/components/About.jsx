ffddf5dac14d94a16399e3833e0b142524622b47
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-04-02 20:01:47 +0200

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,9 +7,7 @@ var About = (props) => {
       <h1 className='text-center'>About</h1>
       <p className='text-center'>Made in 2017 on a React Course</p>
       <p className='text-center'><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
-      <button className="button">
-        <Link to='/'>Return Home</Link>
-      </button>
+      <p><Link to='/'>Return Home</Link></p>
     </div>
   )
 };
