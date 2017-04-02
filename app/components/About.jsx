f487368fc55b288d3a9cabb1dccbd8bf5fef4c44
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-04-02 19:44:34 +0200

    Added style to About/Weather/WeatherForm/WeatherMessage

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className='text-center'>About</h1>
+      <p>Made in 2017 on a React Course</p>
+      <p><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
+      <Link to='/'>Return Home</Link>
     </div>
   )
 };
