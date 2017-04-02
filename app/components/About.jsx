4971f298f5fc9df4d69ee1871a38557d02e4d03c
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-04-02 19:46:56 +0200

    Centered tags in About component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,9 +5,9 @@ var About = (props) => {
   return (
     <div>
       <h1 className='text-center'>About</h1>
-      <p>Made in 2017 on a React Course</p>
-      <p><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
-      <Link to='/'>Return Home</Link>
+      <p className='text-center'>Made in 2017 on a React Course</p>
+      <p className='text-center'><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
+      <Link className='text-center' to='/'>Return Home</Link>
     </div>
   )
 };
