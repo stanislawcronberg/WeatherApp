4f1349047fcc72d891b31f85aa025a3817734097
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-04-02 19:50:07 +0200

    Changed link to button in About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,11 +3,13 @@ var {Link} = require('react-router');
 
 var About = (props) => {
   return (
-    <div>
+    <div className='text-center'>
       <h1 className='text-center'>About</h1>
       <p className='text-center'>Made in 2017 on a React Course</p>
       <p className='text-center'><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
-      <Link className='text-center' to='/'>Return Home</Link>
+      <button className="button">
+        <Link to='/'>Return Home</Link>
+      </button>
     </div>
   )
 };
