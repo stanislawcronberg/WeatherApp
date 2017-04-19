d7e6c5933de07a975dc829370864c5c5270b9d07
Stanislaw Cronberg <stanislawcronberg@protonmail.com>
2017-04-19 09:22:49 +0200

    Added custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var {Link} = require('react-router');
 
 var About = (props) => {
   return (
-    <div className='text-center'>
+    <div className='text-center page-title'>
       <h1 className='text-center'>About</h1>
       <p className='text-center'>Made in 2017 on a React Course</p>
       <p className='text-center'><a href="https://github.com/stanislawcronberg/WeatherApp" target='_blank'>GitHub Repo</a></p>
